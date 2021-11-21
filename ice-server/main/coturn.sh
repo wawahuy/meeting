018b53035f3f9753713e582f157900e0d19926ff
Gia Huy <nguyengiahuy16@gmail.com>
2021-11-21 21:08:53 +0700

    fix docker

--- ice-server/main/coturn.sh
+++ ice-server/main/coturn.sh
@@ -28,3 +28,5 @@ REALM="call.zayuh.me"
 #--cli-port=$PORT_CLI --cli-password=$PASSWORD_CLI --cli-ip=0.0.0.0
 turnserver -a -o -v -n  --no-dtls --no-tls -p $PORT_TURN -r $REALM --min-port=$PORT_MIN --max-port=$PORT_MAX
 
+turnadmin -a -u a_admin -r call.zayuh.me -p a_admin_pwd
+turnadmin -a -u b_admin -r call.zayuh.me -p b_admin_pwd
