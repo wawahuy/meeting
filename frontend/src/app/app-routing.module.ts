cea14452ca37ee60f30e2f20762e703cac1078a7
heyday <hungthinh.ckc@gmail.com>
2021-11-27 23:10:57 +0700

    Updated: UI login form. Added: Sign up page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'test-webrtc', loadChildren: () => import('./test-webrtc/test-webrtc.module').then(m => m.TestWebrtcModule) },
   { path: 'test-peer', loadChildren: () => import('./test-peer/test-peer.module').then(m => m.TestPeerModule) },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }
 ];
 
 @NgModule({
