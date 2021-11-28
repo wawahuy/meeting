82831df41ea06a81b135c8168a75aef971e67400
heyday <hungthinh.ckc@gmail.com>
2021-11-29 01:17:06 +0700

    Added: Message page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: 'test-webrtc', loadChildren: () => import('./test-webrtc/test-webrtc.module').then(m => m.TestWebrtcModule) },
   { path: 'test-peer', loadChildren: () => import('./test-peer/test-peer.module').then(m => m.TestPeerModule) },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) }
+  { path: 'sign-up', loadChildren: () => import('./sign-up/sign-up.module').then(m => m.SignUpModule) },
+  { path: 'message', loadChildren: () => import('./message/message.module').then(m => m.MessageModule) }
 ];
 
 @NgModule({
