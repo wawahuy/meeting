3e61315b0fb6a2c8c2a19b2de74469b15dcf50d4
heyday <hungthinh.ckc@gmail.com>
2021-12-12 22:13:44 +0700

    Updated: Binding data search room

--- frontend/src/app/_services/room.service.ts
+++ frontend/src/app/_services/room.service.ts
@@ -1,28 +1,50 @@
+import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Room } from '../_models/room';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class RoomService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor(private http: HttpClient) { }
+  getRoomName(room: Room) {
+    if (!!room.roomDetail.name) {
+      return room.roomDetail.name;
+    }
+    if (room.roomDetail.users.length >= 3) {
+      return room.roomDetail.users
+        .filter(
+          (item) => item.user._id !== this.authService.currentUserValue._id
+        )
+        .map((item) => item.user.name)
+        .join(', ');
+    } else
+      return room.roomDetail.users
+        .filter(
+          (item) => item.user._id !== this.authService.currentUserValue._id
+        )
+        .map((item) => {
+          return { username: item.user.username, name: item.user.name };
+        });
+  }
 
   search(search: string, page: number = 1, size: number = 10) {
     const url = environment.apiEndpoint + 'room/search';
-    const param : { [key: string]: string } = {
+    const param: { [key: string]: string } = {
       page: page.toString(),
-      size: size.toString()
-    }
+      size: size.toString(),
+    };
     if (search) {
       param['search'] = search;
     }
-    return this.http.get<Room[]>(url, {params: param, ...httpOptions}).toPromise();
+    return this.http
+      .get<Room[]>(url, { params: param, ...httpOptions })
+      .toPromise();
   }
 }
