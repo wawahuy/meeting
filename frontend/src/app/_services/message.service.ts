a96212dc360a8d5507b4c96f6f2cb7f3768b398a
heyday <hungthinh.ckc@gmail.com>
2021-12-20 23:11:04 +0700

    Updated: API message

---
+++ frontend/src/app/_services/message.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Message } from '../_models/message';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  constructor(private http: HttpClient) {}
+
+  getMessagesByRoomId(roomId, search, page, size) {
+    const url = environment.apiEndpoint + 'message/search';
+    const params: { [key: string]: string } = {
+      roomId: roomId.toString(),
+      search: search.toString(),
+      page: page.toString(),
+      size: size.toString(),
+    };
+
+    return this.http
+      .get<Message>(url, { params: params, ...httpOptions })
+      .toPromise();
+  }
+}
