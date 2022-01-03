3815f5d91f33b66297d3854dcde754f5a1eedd22
heyday <hungthinh.ckc@gmail.com>
2022-01-03 16:29:40 +0700

    Updated: Modal Profile

--- frontend/src/app/_components/modal-profile/modal-profile.component.ts
+++ frontend/src/app/_components/modal-profile/modal-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
 import { RoomService } from 'src/app/_services/room.service';
 
 @Component({
@@ -11,19 +12,29 @@ export class ModalProfileComponent implements OnInit {
   isShow = false;
 
   nickName: String;
+  user: User;
 
   constructor(private roomService: RoomService) {}
 
   ngOnInit(): void {}
 
-  public show() {
+  public show(user: User) {
     this.isShow = true;
+    this.user = user;
   }
   public hidden() {
     this.isShow = false;
   }
 
   getUser() {
+    if (!!this.user) {
+      const user = this.roomCurrent.users.find(
+        (item) => item.user._id === this.user._id
+      );
+      console.log(user.user.name);
+
+      return user.user;
+    }
     const user = this.roomService.getRoomName(this.roomCurrent);
     return user;
   }
